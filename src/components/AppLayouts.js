f00b25a25a3de82313ff53b0689703c6df579d0a
김민성 <yssj2049@gmail.com>
2021-02-12 10:50:42 +0900

    Add basic layout in index.js

--- src/components/AppLayouts.js
+++ src/components/AppLayouts.js
@@ -1,12 +1,129 @@
 import React from 'react';
 
+import { Col, Row, Layout, Menu, Breadcrumb, List, Card} from 'antd'
+import {css} from '@emotion/react';
+import Banner from './Banner';
+import Footer from './Footer';
+
+const {Header, Content} = Layout;
+const data = [
+  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },
+];
 export default function AppLayouts(){ 
 
+
   return (
-    <div>
-      <h1>
-        배포 테스트
-      </h1>
-    </div>
-  );
+    <>
+    <Layout style={{height:"100vh", overflow:"auto"}}>
+      <Header>Header</Header>
+      <Content>
+        <Banner/>
+        <List
+          style={{display:"inline-block", marginTop:"30px"}}
+          grid ={{gutter:16, column :4}}
+          dataSource={data}
+          renderItem={item=>(
+            <List.Item>
+              <Card
+                  hoverable
+                  bodyStyle={{padding: "0"}}
+              cover ={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}/>
+            </List.Item>
+          )}
+          >
+
+        </List>
+        <Footer>Footer</Footer>
+      </Content>
+    </Layout>
+  </>
+    );
 }
